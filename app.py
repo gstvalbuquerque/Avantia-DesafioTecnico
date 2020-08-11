1c87cc1ff4c44484401ead1f9fa6484f241f6008
Gustavo Sales <gustavo.sales87@hotmail.com>
2020-08-11 02:12:35 -0300

    Updated Register

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 #configuração do banco de dados
-conn = sqlite3.connect('investsimples.db', check_same_thread=False)
+conn = sqlite3.connect('avantia-sma.db', check_same_thread=False)
 c = conn.cursor()
 
 #criar o server
@@ -38,7 +38,50 @@ def home():
 #registrar
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+
+    else:
+        # criar tabela caso não exista
+        c.execute("CREATE TABLE IF NOT EXISTS accounts ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' VARCHAR(50) NOT NULL, 'email' VARCHAR(250) NOT NULL, 'hash' VARCHAR(250) NOT NULL)")
+
+        #pegar os dados do formulário
+        username = request.form.get("username")
+        password = request.form.get("password")
+        conf_password = request.form.get("conf_password")
+        email = request.form.get("email")
+
+        #conferir se as senhas conferem
+        if (password != conf_password):
+            flash("As senhas não são iguais!")
+            return render_template("register.html")
+        
+        #checar na base de dados se já existe alguém com o mesmo login
+        rows = c.execute("SELECT * FROM accounts WHERE username = (?)",
+                          [username])
+        conf_user = rows.fetchone()
+        if conf_user is not None:
+            flash("Nome de usuário já cadastrado!")
+            return render_template("register.html")
+
+        #chegar na base de dados se já existe alguém com o mesmo email
+        email_row = c.execute("SELECT * FROM accounts WHERE email = (?)",
+                          [email])
+        conf_email = email_row.fetchone()
+        if conf_email != None:
+            flash("Email já cadastrado")
+            return render_template("register.html")
+        
+        #inserir no banco de dados
+        password=generate_password_hash(password) 
+        c.execute("INSERT INTO accounts (username,email, hash) VALUES (?, ?, ?)", (username, email, password))
+        conn.commit() 
+        flash("Registrado com sucesso!")
+        return render_template("login.html")
+
+        redirect ("/login")
+
+
 
 # fazer o login
 @app.route("/login", methods=["GET", "POST"])
